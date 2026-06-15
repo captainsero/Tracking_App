96df6ddffa2e39b2295c2006792af22ed030f725
MedoAbdallah <mohamedmedo2282004a@gmail.com>
2026-06-15 15:00:02 +0300

    update(app-sections):update nagvigation and app router

--- lib/core/router/nav_helper.dart
+++ lib/core/router/nav_helper.dart
@@ -1,30 +1,27 @@
-// import 'package:flutter/material.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:tracking_app/core/router/route_path.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tracking_app/core/router/route_path.dart';
 
-// class NavHelper {
-//   static int getCurrentIndex(String location) {
-//     if (location == RoutePath.home) return 0;
-//     if (location == RoutePath.categories) return 1;
-//     if (location == RoutePath.cart) return 2;
-//     if (location == RoutePath.profile) return 3;
-//     return 0;
-//   }
+class NavHelper {
+  static int getCurrentIndex(String location) {
+    if (location == RoutePath.home) return 0;
+    if (location == RoutePath.orders) return 1;
+    if (location == RoutePath.profile) return 2;
+    return 0;
+  }
 
-//   static void onItemTapped(BuildContext context, int index) {
-//     switch (index) {
-//       case 0:
-//         context.go(RoutePath.home);
-//         break;
-//       case 1:
-//         context.go(RoutePath.categories);
-//         break;
-//       case 2:
-//         context.go(RoutePath.cart);
-//         break;
-//       case 3:
-//         context.go(RoutePath.profile);
-//         break;
-//     }
-//   }
-// }
+  static void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go(RoutePath.home);
+        break;
+      case 1:
+        context.go(RoutePath.orders);
+        break;
+
+      case 2:
+        context.go(RoutePath.profile);
+        break;
+    }
+  }
+}
