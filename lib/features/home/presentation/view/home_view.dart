83401bf016255e3d135d9f66d9146a27fb50d95b
Yasser Samir <yasoo976098@gmail.com>
2026-06-21 14:23:05 +0300

    feat(chore): impl home feature layers

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,24 +1,9 @@
-
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-import '../../../../core/router/nav_helper.dart';
-import '../../../../core/shared_widgets/custom_buttom_navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final String location = GoRouterState.of(context).uri.toString();
-
-    return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: const Center(child: Text("Home View")),
-      bottomNavigationBar: CustomBottomNavigationBar(
-        currentIndex: NavHelper.getCurrentIndex(location),
-        onTap: (index) => NavHelper.onItemTapped(context, index),
-      ),
-    );
+    return Scaffold(body: Center(child: Text('home View')));
   }
 }
